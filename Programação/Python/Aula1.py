3db332d31c35a4abbb4a1a7e5f0879d020afd874
Morpheu0 <153289276+Morpheu0@users.noreply.github.com>
2024-04-01 17:12:29 -0300

    Add files via upload

---
+++ Programação/Python/Aula1.py
@@ -0,0 +1,18 @@
+#Variaveis.
+
+x = 5 #É uma variável númerica do tipo int (inteiro). Ex: 1,2,3,4,5
+
+y = 1.0 #É uma variável númerica do tipo float (decimais). Ex: 1.0, 2.0, 3.0
+
+z = "Jhon" #É uma variável do tipo str (string). Ex: 'Jonas', 'Mário', "Maria", "Josefa"
+
+#Pode-se usar o type para mostrar o tipo de varíavel da variável.
+
+print(type(z))
+
+#Sintaxe - Maneira coreta de se programar em Python.
+
+x = 3
+y = 4
+if x+y == 7:
+    print("Correto")
